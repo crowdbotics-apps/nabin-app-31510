35e634540dfe39ea5a1f304806e0e52a7867222b
Crowdbotics <team@crowdbotics.com>
2021-10-18 14:04:24 +0000

    Initial commit.

---
+++ nabin_app_31510/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nabin_app_31510 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nabin_app_31510.settings')
+
+application = get_wsgi_application()
